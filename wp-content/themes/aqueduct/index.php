7dda3a41525bed3bf948a6c05ee5f1e65d8711eb
Branndon Arruda <branndon.arruda@yahoo.com.br>
2016-04-30 13:28:36 -0300

    Instalação inicial

---
+++ wp-content/themes/aqueduct/index.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * @package HowlThemes
+ */
+
+get_header(); ?>
+
+
+<div class="container content-mosaico">
+	<div class="grid_6 feat-post">
+		<ul>
+			<li>
+				<a href="#" target="_blank">
+					<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-img1.jpg" />
+					<span class="maven_promedium">Cultura</span>
+					<h3 class="maven_probold">Playlist de verão!</h3>
+					<div class="border"></div>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="grid_6 second-list">
+		<ul>
+			<li class="grid_3">
+				<a href="#" target="_blank">
+					<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-img2.jpg" />
+					<span class="maven_promedium">Receitas</span>
+					<h3 class="maven_probold">(Bons) sonhos de uma noite de verão</h3>
+					<div class="border"></div>
+				</a>
+			</li>
+			<li class="grid_3">
+				<a href="#" target="_blank">
+					<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-img3.jpg" />
+					<span class="maven_promedium">Beleza</span>
+					<h3 class="maven_probold">Cuidando dos cabelos no verão</h3>
+					<div class="border"></div>
+				</a>
+			</li>
+			<li class="grid_3">
+				<a href="#" target="_blank">
+					<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-img4.jpg" />
+					<span class="maven_promedium">Beleza</span>
+					<h3 class="maven_probold">5 dicas milagrosas para se livrar da ressaca</h3>
+					<div class="border"></div>
+				</a>
+			</li>
+			<li class="grid_3">
+				<a href="#" target="_blank">
+					<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-img5.jpg" />
+					<span class="maven_promedium">Cultura</span>
+					<h3 class="maven_probold">Pólen: a superfood das abelhas</h3>
+					<div class="border"></div>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<div class="clear-both"></div>
+
+<div class="container content-home">
+
+	<!-- SLIDER + CONTENT - LEFT -->
+	<div class="content-left grid_7">
+		<div class="content-slider">
+			<ul class="bxslider">
+				<li>
+					<a href="#">
+						<span>Moda</span>
+						<h3>Três peças, três hits do verão 2016</h3>
+						<div class="border"></div>
+						<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-slider.jpg" alt="<?php bloginfo( 'name' ); ?>"/>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<span>Moda</span>
+						<h3>Três peças, três hits do verão 2016</h3>
+						<div class="border"></div>
+						<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-slider.jpg" alt="<?php bloginfo( 'name' ); ?>"/>
+					</a>
+				</li>
+			</ul>
+		</div>
+		
+		<!-- ULTIMOS POSTS -->
+		<div class="content-last-posts">
+			<div class="content-title-posts first green-border">
+				<h3 class="maven_promedium">Últimas postagens</h3>
+			</div>
+
+			<!-- <ul>
+				<li>
+					<a>
+						<div class="content-img">
+							<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-post.jpg" alt="<?php bloginfo( 'name' ); ?>"/>
+						</div>
+						<div class="content">
+							<div class="border">
+								<h4 class="maven_promedium">Queimei demais. E agora?</h4>
+								<span class="maven_promedium date">29 DE JANEIRO DE 2016</span>
+								<span class="maven_promedium category">beleza</span>
+							</div>
+							<div class="clear"></div>
+							<p class="text lato">Andou abusando do Sol ou da piscina e agora está sentindo as consequências na pele? Calma, nós vamos te ajudar. Confira aqui as nossas dicas quentes (e que não ardem) para aliviar os efeitos do <span>[...] Continuar lendo.</span></p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a>
+						<div class="content-img">
+							<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-post.jpg" alt="<?php bloginfo( 'name' ); ?>"/>
+						</div>
+						<div class="content">
+							<div class="border">
+								<h4 class="maven_promedium">Queimei demais. E agora?</h4>
+								<span class="maven_promedium date">29 DE JANEIRO DE 2016</span>
+								<span class="maven_promedium category">beleza</span>
+							</div>
+							<div class="clear"></div>
+							<p class="text lato">Andou abusando do Sol ou da piscina e agora está sentindo as consequências na pele? Calma, nós vamos te ajudar. Confira aqui as nossas dicas quentes (e que não ardem) para aliviar os efeitos do <span>[...] Continuar lendo.</span></p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a>
+						<div class="content-img">
+							<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-post.jpg" alt="<?php bloginfo( 'name' ); ?>"/>
+						</div>
+						<div class="content">
+							<div class="border">
+								<h4 class="maven_promedium">Queimei demais. E agora?</h4>
+								<span class="maven_promedium date">29 DE JANEIRO DE 2016</span>
+								<span class="maven_promedium category">beleza</span>
+							</div>
+							<div class="clear"></div>
+							<p class="text lato">Andou abusando do Sol ou da piscina e agora está sentindo as consequências na pele? Calma, nós vamos te ajudar. Confira aqui as nossas dicas quentes (e que não ardem) para aliviar os efeitos do <span>[...] Continuar lendo.</span></p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a>
+						<div class="content-img">
+							<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-post.jpg" alt="<?php bloginfo( 'name' ); ?>"/>
+						</div>
+						<div class="content">
+							<div class="border">
+								<h4 class="maven_promedium">Queimei demais. E agora?</h4>
+								<span class="maven_promedium date">29 DE JANEIRO DE 2016</span>
+								<span class="maven_promedium category">beleza</span>
+							</div>
+							<div class="clear"></div>
+							<p class="text lato">Andou abusando do Sol ou da piscina e agora está sentindo as consequências na pele? Calma, nós vamos te ajudar. Confira aqui as nossas dicas quentes (e que não ardem) para aliviar os efeitos do <span>[...] Continuar lendo.</span></p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a>
+						<div class="content-img">
+							<img src="<?php bloginfo( 'template_directory' ); ?>/img/teste-post.jpg" alt="<?php bloginfo( 'name' ); ?>"/>
+						</div>
+						<div class="content">
+							<div class="border">
+								<h4 class="maven_promedium">Queimei demais. E agora?</h4>
+								<span class="maven_promedium date">29 DE JANEIRO DE 2016</span>
+								<span class="maven_promedium category">beleza</span>
+							</div>
+							<div class="clear"></div>
+							<p class="text lato">Andou abusando do Sol ou da piscina e agora está sentindo as consequências na pele? Calma, nós vamos te ajudar. Confira aqui as nossas dicas quentes (e que não ardem) para aliviar os efeitos do <span>[...] Continuar lendo.</span></p>
+						</div>
+					</a>
+				</li>
+			</ul> -->
+			<!-- LISTA OS POSTS - PARAMETRO: CATEGORIA -->
+			<?php lista_posts_home($catID); ?>
+		</div>
+
+	</div>
+
+	<!-- SIDEBAR - RIGHT -->
+	<div class="content-right most-view grid_5">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+
+<div class="clear"></div>
+
+
+<?php
+	// if(get_theme_mod("home_display") == 'magazine'){
+	// 	get_template_part( 'magazine', get_post_format() );
+	// }
+	// else {
+	// 	get_template_part( 'blog', get_post_format() );
+	// }
+?>
+<?php // get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
